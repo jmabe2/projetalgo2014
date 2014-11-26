1f7890be14805ca0f2506cbc43ddfe84cbebccb2
jmabe2 <jean-michel.abe@promsocatc.net>
2014-11-26 20:01:05 +0100

    Update jm.c

--- jm.c
+++ jm.c
@@ -1,1 +1,39 @@
-nouveaufichier
+int verifstrformat (char* format, char* chaine)
+{
+	int i, res;
+	res = 1;
+	if (strlen(format) == strlen(chaine))
+	{
+		for (i=0;i<strlen(chaine);i++)
+		{
+			switch(format[i])
+			{
+				case '#' : if ((chaine[i] < 48) || (chaine[i] > 57))
+				           {
+							   res=0;
+						   }
+						   break;
+			    default : if (chaine[i] != format[i])
+			               {
+							   res=0;
+						   }
+			}
+		}
+    }
+    else
+    {
+		res=0;
+    }
+    return res;
+}
+
+
+int verifnumregnat (char* chaine)
+{
+	return verifstrformat("######-###-##", chaine);
+} 
+
+int verifnumcompte (char* chaine)
+{
+	return verifstrformat("BE## #### #### ####", chaine);
+} 
